653c3ea42b595034977e5a7276908dd185999dcf
davxe <princekaushal14918177h@gmail.com>
2020-10-16 16:18:33 +0530

    add header and question form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import Header from './component/Header';
+import QuestionForm from './component/Form';
 function App() {
   return (
     <div className="App">
-    
+      <Header/>
+      <QuestionForm/>
     </div>
   );
 }
